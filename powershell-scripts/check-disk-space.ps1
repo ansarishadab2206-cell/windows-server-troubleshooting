b94933f6dc68360ed94019eb1a1474208094f60a
ansarishadab2206-cell <ansari.shadab2206@gmail.com>
2026-01-13 19:34:33 +0530

    Create check-disk-space.ps1

---
+++ powershell-scripts/check-disk-space.ps1
@@ -0,0 +1,6 @@
+# Check disk space on server
+Get-PSDrive -PSProvider FileSystem |
+Select-Object Name,
+@{Name="UsedGB";Expression={[math]::Round($_.Used/1GB,2)}},
+@{Name="FreeGB";Expression={[math]::Round($_.Free/1GB,2)}}
+
